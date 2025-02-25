874a75371ca5618a1ad4e26c521d19a5c57f8add
Davoad Eivazkhani <75331169+Davoadeivai@users.noreply.github.com>
2025-02-25 22:44:24 +0330

    Update views.py

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework import serializers
 from .models import Product
 from .models import Category
+from .models import Cart
 
 #The home page view usually displays all the main products or categories.
 def home(request):
@@ -19,6 +20,14 @@ ef category_view(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     products = category.product_set.all()  # محصولات این دسته‌بندی را دریافت می‌کنیم
     return render(request, 'shop/category.html', {'category': category, 'products': products})
+
+# ویو سبد خرید نمایش محصولات انتخابی کاربر در سبد خرید را انجام می‌دهد.
+# Shopping cart view displays the products selected by the user in the shopping cart.
+def cart_view(request):
+    cart = Cart.objects.get(user=request.user)  # سبد خرید کاربر جاری
+    return render(request, 'shop/cart.html', {'cart': cart})
+
+
 
 
 
