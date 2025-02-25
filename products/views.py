19f018270020336e92405523764f58af4ed52f94
Davoad Eivazkhani <75331169+Davoadeivai@users.noreply.github.com>
2025-02-25 22:37:41 +0330

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from rest_framework import serializers
+from .models import Product
 
-# Create your views here.
+#The home page view usually displays all the main products or categories.
+def home(request):
+    products = Product.objects.all()  # نمایش تمام محصولات
+    return render(request, 'shop/home.html', {'products': products})
+
+
+
