66825804e9531ac98665b6f7afce0d4971140c2b
amir <amirsimorgh79@gmail.com>
2025-03-03 17:32:08 +0330

    change the product model

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from users.models import MemberModel
-from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from taggit.managers import TaggableManager
 
@@ -12,24 +11,40 @@ from taggit.managers import TaggableManager
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    unit_price = models.IntegerField()
+    price = models.IntegerField()
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    stock = models.PositiveIntegerField()
+    units_sold = models.PositiveIntegerField()
     active = models.BooleanField(default=True)
-    tags = TaggableManager()  
+    tags = TaggableManager()
+    text_content = models.TextField(null=True, blank=True)
+    users = models.ManyToManyField(MemberModel, related_name='users')
 
     def __str__(self):
         return self.name
-
-
+    
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='media/product/images')
+    image = models.ImageField(upload_to='product/images')
 
     def __str__(self):
         return f"Image for {self.product.name}"
 
+
+class ProductVideo(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='videos')
+    video = models.FileField(upload_to='product/videos')
+
+    def __str__(self):
+        return f"Video for {self.product.name}"
+
+
+class ProductAudio(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='audios')
+    audio = models.FileField(upload_to='product/audios')
+
+    def __str__(self):
+        return f"Audio for {self.product.name}"
 
 
 
@@ -44,4 +59,4 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Review for {self.product.name} by {self.user.username}"
+        return f"coment for {self.product.name} by {self.user.username}"
