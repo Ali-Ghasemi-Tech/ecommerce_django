275b684f2cdf7f66ca009bc19e93123d29957f3d
Ali-Ghasemi-Tech <thelastblade30@gmail.com>
2025-03-26 18:29:45 +0000

    move docs url to root

--- config/urls.py
+++ config/urls.py
@@ -32,13 +32,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    
+
     path("admin/", admin.site.urls),
     path('api/order/' , include('order.urls')),
     path('api/products/' , include('products.urls')),
     path('api/account/' , include('account.urls')),
     path('api/payment/' , include('payment.urls')),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
 
 ]
  # اضافه کردن ادرس های دیباگ تولبار
